56a878f2461540a5505a364e867a6076f55c06fc
whworjs7946 <whworjs7946@naver.com>
2019-06-27 15:46:40 +0900

    Create Fragment2.java

---
+++ app/src/main/java/com/example/task1/ui/main/tab2/Fragment2.java
@@ -0,0 +1,49 @@
+package com.example.task1.ui.main.tab2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import androidx.fragment.app.Fragment;
+import com.example.task1.R;
+
+public class Fragment2 extends Fragment {
+
+  GridView gridViewImages;
+
+  public int[] imageIDs = new int[]{
+      R.drawable.image_01,
+      R.drawable.image_02,
+      R.drawable.image_03,
+      R.drawable.image_04,
+      R.drawable.image_05,
+      R.drawable.image_06,
+      R.drawable.image_07,
+      R.drawable.image_08,
+      R.drawable.image_09,
+      R.drawable.image_10,
+
+
+  };
+
+
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.layout_fragment2, container, false);
+
+    gridViewImages = (GridView) view.findViewById(R.id.gridViewImages);
+    ImageGridAdapter imageGridAdapter = new ImageGridAdapter(this.getContext(), imageIDs);
+    gridViewImages.setAdapter(imageGridAdapter);
+
+    return view;
+
+  }
+
+
+}
