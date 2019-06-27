bde89f33bbde2b8c2a79b5d6f952acc75cd78f53
whworjs7946 <whworjs7946@naver.com>
2019-06-27 11:07:12 +0900

    Initial commit

---
+++ app/src/main/java/com/example/task1/ui/main/Fragment1.java
@@ -0,0 +1,21 @@
+package com.example.task1.ui.main;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import com.example.task1.R;
+
+public class Fragment1 extends Fragment {
+
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.layout_fragment1, container, false);
+  }
+}
