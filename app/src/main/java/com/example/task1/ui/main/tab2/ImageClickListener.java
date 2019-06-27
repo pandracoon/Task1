32afa1f9ded862b4bedfd0a6d46bd7f994b01aae
whworjs7946 <whworjs7946@naver.com>
2019-06-27 15:46:43 +0900

    Create ImageClickListener.java

---
+++ app/src/main/java/com/example/task1/ui/main/tab2/ImageClickListener.java
@@ -0,0 +1,23 @@
+package com.example.task1.ui.main.tab2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class ImageClickListener implements OnClickListener {
+
+  Context context;
+  int imageID;
+
+  public ImageClickListener(Context context, int imageID){
+    this.context=context;
+    this.imageID=imageID;
+  }
+
+  public void onClick(View v){
+    Intent intent = new Intent(context, ImageActivity.class);
+    intent.putExtra("image ID", imageID);
+    context.startActivity(intent);
+  }
+}
