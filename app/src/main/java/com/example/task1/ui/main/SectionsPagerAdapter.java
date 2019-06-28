d3780d951a7d41f1b20a8c4af5f546e6e899097e
vm3fk5dla2 <52191701+vm3fk5dla2@users.noreply.github.com>
2019-06-28 17:05:13 +0900

    RecyclerView_Contacts.ver1

--- app/src/main/java/com/example/task1/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/task1/ui/main/SectionsPagerAdapter.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import com.example.task1.R;
+import com.example.task1.Tab1.Fragment1;
 
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
